bcf8d50a596456af016c34cf40e9c96b17ce9f0a
CASSCODE92 <67381271+CASSCODE92@users.noreply.github.com>
2020-07-04 18:56:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const lrrh = {
+    color: color
+    shape:
+    size:
+}
+
+
+
+
+var layout, mouseLocation, square, lrrh, wolf, gameLoop, sizeAdjust
+// lrrh= little red riding hood square
+// gameLoop = make the functions loop
+// make elements adjust in size relative to adjusted browser
