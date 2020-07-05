810367adffca5f4371a7c8c9d0d1b3ddb0f5be00
CASSCODE92 <cassie2012abioye@gmail.com>
2020-07-05 16:44:49 -0400

    script update

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-const lrrh = {
-    color: color
-    shape:
-    size:
-}
+Player
 
 
-
-
-var layout, mouseLocation, square, lrrh, wolf, gameLoop, sizeAdjust
-// lrrh= little red riding hood square
-// gameLoop = make the functions loop
-// make elements adjust in size relative to adjusted browser
+square = {}
